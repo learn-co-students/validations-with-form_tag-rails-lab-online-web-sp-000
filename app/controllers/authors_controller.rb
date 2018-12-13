11c92d9fdbf17b973253c4e24b13057d309644f5
wingedmachine <wheeler.pryor@gmail.com>
2018-12-13 15:10:39 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,48 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update]
+  before_action :form_title, only: [:new, :edit]
+
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
     @author = Author.new
+    render :new
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      form_title
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    render :new
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      form_title(@author.name)
+      render :new
+    end
   end
 
   private
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def form_title(author_name: nil)
+    @form_title = author_name ? "Editing #{author_name}" : "Create an Author"
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,43 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+  before_action :form_title, only: [:new, :edit]
+
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+    @form_name = "Create a Post"
+    render :edit
+  end
+
+  def create
+    # this won't be tested, so I'm not writing it
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @form_name = "Editing #{@post.title}"
+    render :edit
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      form_title
+      render :edit
+    end
   end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def form_title(post_title: nil)
+    @form_title = post_title ? "Editing #{post_title}" : "Create an Author"
   end
 end
