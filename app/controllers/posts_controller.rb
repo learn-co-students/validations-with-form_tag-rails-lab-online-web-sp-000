a92b33d08478673ec203bb453d55db1cfc3c702e
Diana Badas <diana.badas@gmail.com>
2019-05-28 17:02:19 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,16 +13,22 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
