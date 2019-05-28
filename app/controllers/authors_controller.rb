90028bb359e5c71ba0a13fae909498de4ae10d43
Diana Badas <diana.badas@gmail.com>
2019-05-28 10:47:14 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,12 +7,20 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def create
+    @author = Author.create(author_params)
+
+    redirect_to author_path(@author)
+  end
+
   def edit
     @author = Author.find(params[:id])
   end
 
-  def create
-    @author = Author.create(author_params)
+  def update
+    @author = Author.find(params[:id])
+
+    @author.update(author_params)
 
     redirect_to author_path(@author)
   end
