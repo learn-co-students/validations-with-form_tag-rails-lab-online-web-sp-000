b34ddf7b01581dd1028f6b32a7363704dc1bfe96
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 13:43:59 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -17,6 +17,23 @@ class AuthorsController < ApplicationController
     end
   end
 
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author.update(author_params)
+
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def author_params
