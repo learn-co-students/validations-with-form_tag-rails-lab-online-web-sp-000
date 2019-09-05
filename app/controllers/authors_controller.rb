a2333900abd31ff9176eee089a2406a20cf43d3d
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-05 15:24:28 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,16 +6,28 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
+  
+  def edit
+  end
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+  
+  def update
+  end
+  
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
   end
+  
 end
