4200745391ab284bebadce72f2965f4decc9d24c
Malcolm <echelon1089@gmail.com>
2019-10-17 04:21:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
     validates :title, presence: true
     validates :content, presence: true, length: { :minimum => 100 }
-    validates :category, presence: true  
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
    
 
 end
