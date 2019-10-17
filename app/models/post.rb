708dc27fe01c64be828f2eb1bd8e2997cbec98a8
Malcolm <echelon1089@gmail.com>
2019-10-16 22:49:07 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
     validates :title, presence: true
-    validates :content, presence: true 
+    validates :content, presence: true, length: { :minimum => 100 }
     validates :category, presence: true  
    
 
