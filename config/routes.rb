fd68b42bebec98529346141eed78adaf0f62d05c
rohanphillips <rohan@solutions-insurance.com>
2019-11-15 14:11:15 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:new, :edit, :create, :update, :show]
+  resources :posts, only:[:new, :edit, :create, :update, :show]
 end
