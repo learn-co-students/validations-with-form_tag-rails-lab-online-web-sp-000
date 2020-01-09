6a8be6b868c935c16456c3238fa2156fbc462ded
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-09 18:55:42 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,8 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update]
+
   def show
-    @author = Author.find(params[:id])
+
   end
 
   def new
@@ -10,10 +12,29 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      #re-render the :new template WITHOUT throwing
+      # away the invalid @author
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
   end
 
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
