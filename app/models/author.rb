525f854a130cc901dd71e0e53a35e6b8f5f97c1f
Christopher Dent <christopherdent01@gmail.com>
2020-02-08 20:30:12 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+
+  validates :name, presence: { message: "must be given please" }
+  validates :email, uniqueness: { message: "Not a unique email" }
+  validates :phone_number, length: { is: 10, message: "Must be 10 digits" }
+  #validates :phone_number, numericality: true
+
+  validates :phone_number, format: { without: /\A[a-zA-Z]+\z/, message: "only allows numbers" }
+
 end
