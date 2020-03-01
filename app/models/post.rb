277a41214d00f190c4a97a84b8c669afab504814
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-03-01 19:56:45 +0000

    add validates for title, content and category

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 100} #content is at least 100 characters long
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)} #category is either "Fiction" or "Non-Fiction"
 end
