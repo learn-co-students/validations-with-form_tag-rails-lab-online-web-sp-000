640dc717898f520d2122001ba4d020e3d3d6427f
programmingmanzo <programmingmanzo@gmail.com>
2020-04-08 21:06:30 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,12 +7,22 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def edit
+  end 
+  
   def create
-    @author = Author.create(author_params)
-
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save 
     redirect_to author_path(@author)
+    else 
+      render :new 
+    end
   end
 
+  def update 
+  end 
+  
   private
 
   def author_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, except: [:destroy, :index]
+  resources :posts, except: [:destroy, :index]
 end
