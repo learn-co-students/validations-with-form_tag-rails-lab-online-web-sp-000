7ac7168020500d638a07862eac305ac6b0d03c98
Michael Carroll <carrollm2@gmail.com>
2020-05-27 03:06:37 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
-  patch "/posts/:id", to: "posts#update"  
+  patch "/posts/:id", to: "posts#update"
+
 end
