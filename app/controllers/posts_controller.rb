6c3fe67ab29fed870988703adb3d0667fc41fc82
yumacorn <reachryanmui@gmail.com>
2020-06-11 00:29:46 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,8 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post_update = Post.new(post_params)
-    if @post_update.valid?
-      @post.update(post_params)
+    # @post_update = Post.new(post_params)
+    if @post.update(post_params)
 
       redirect_to post_path(@post)
     else
