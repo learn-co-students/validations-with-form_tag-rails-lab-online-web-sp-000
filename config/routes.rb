d26648a86cedb3bbf372a3c3b787b9b9727d2a90
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-11 22:30:15 -0700

    add routes for authors and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, :posts
+  # get "/authors/new", to: "authors#new", as: "new_author"
+  # post "/authors", to: "authors#create"
+  # get "/authors/:id", to: "authors#show", as: "author"
 end
