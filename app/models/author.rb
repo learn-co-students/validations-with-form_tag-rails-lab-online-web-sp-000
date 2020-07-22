cc2a531543799aed55bf88be880563f41ccdffaf
Sergio Marelli <sergio.mrll@gmail.com>
2020-07-22 19:04:58 +0100

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, uniqueness: true
+	validates :phone_number, length: { is: 10 }
 end
+# name is not blank
+# email is unique
+# phone_number is exactly 10 digits long
