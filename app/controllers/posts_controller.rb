e70020cb4e7f694ea480f2d5ae4fb3c68ffaad46
marcon29 <online@markstabler.com>
2020-07-27 01:16:47 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
-class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
+class PostsController < ApplicationController  
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def show    
   end
 
-  def edit
-    @post = Post.find(params[:id])
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit    
   end
 
   def update
     @post = Post.find(params[:id])
+    @post.assign_attributes(post_params)
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:title, :category, :content)
