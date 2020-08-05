0c620d40be5b3e6bdb43e275af845c43b8e71cb3
Ronda Cowen <cowenhome5@gmail.com>
2020-08-05 15:13:42 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,5 @@
 class Author < ActiveRecord::Base
     validates :name, presence: true
-    validates :email, presence: true
     validates :email, uniqueness: true
     validates :phone_number, presence: true, length: { is: 10, message: "must be ten numbers long" }
        
