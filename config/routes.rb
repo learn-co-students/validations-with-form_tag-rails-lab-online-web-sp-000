72bd43d2f5c059d0f8dfd835a682abb6d4a63dc2
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-09 07:03:19 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resource :posts, :authors, only: [:show, :new, :edit, :create, :update]
+  get '/authors', to: 'authors#show', as: 'author'
+  get '/posts', to: 'posts#show', as: 'post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
