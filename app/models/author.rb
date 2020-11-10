694e5bd8724d04edb1bc2a63106e0f184363fc6e
dcantoran <davidcantoran36@gmail.com>
2020-11-10 10:33:27 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+    # name != blank
+    validates :name, presence: true
+    # unique :email
+    validates :email, uniqueness: true
+    # phone_number *10 digits long*
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    # title != blank
+    validates :title, presence: true
+    # content at least 100 characters
+    validates :content, length: { minimum: 100 }
+    # category Fiction || Non-Fiction
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
