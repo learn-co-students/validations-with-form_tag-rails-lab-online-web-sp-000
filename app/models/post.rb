fe0eb2c7e1a1584b2f0527e7289cda9decb72c71
rmholsen <jaguar.kitty@gmail.com>
2021-02-04 13:37:22 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 100}
+    validates :category, format: { with: /\A(Fiction)\z|\A(Non-Fiction)\z/}
 end
