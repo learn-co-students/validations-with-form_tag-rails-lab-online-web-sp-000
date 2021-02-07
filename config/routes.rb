d8c4859c551e69fc13ff822ec6a054d769e8cb00
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-07 12:10:53 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :author 
   resources :post
+  resources :authors 
+  resources :posts
 end
