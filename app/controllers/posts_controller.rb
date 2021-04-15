b3b28fb97d90683e06db4ea7f7e8d90199ded308
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-04-15 12:16:32 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
@@ -24,6 +25,7 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     if @post.valid?
+      #binding.pry
       redirect_to post_path(@post)
     else
       render :edit
