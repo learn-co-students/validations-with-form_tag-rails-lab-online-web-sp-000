84f9510661f49b03cf8bac6c970c2239249ec8dd
JustinBreneman <Justin.Breneman@yahoo.com>
2021-05-23 19:53:44 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates_with CategoryValidator
 end
