8950eee5cd5086d05d225326c59eef9ba68e3889
jousethen <jthen93@gmail.com>
2021-06-10 15:13:46 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
